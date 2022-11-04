c5acf8f8a3ac939eac5476c3ccc13ecbab61ae54
Eric Ye <eric.ye@facteon.global>
2022-11-04 21:47:21 +1300

    Fix some display issues

--- display-message.sh
+++ display-message.sh
@@ -5,15 +5,16 @@ source $current_dir/variables.sh
 
 main()
 {
-    message=$(cat $TMUX_NOTIFY_FILE 2>/dev/null)
+    message="$(cat $TMUX_NOTIFY_FILE 2>/dev/null)"
     i=0; placeholder=""
     while [ $i -lt ${#message} ]; do
-        [ $(echo -n "${message:$i:1}" |wc -c) -eq 1 ] && placeholder+="*" || placeholder+="**"
+        [ $(echo -n "${message:$i:1}" |wc -c) -eq 1 ] && placeholder+=" " || placeholder+="  "
         i=$(($i + 1))
     done
     if [ -n "$message" ]; then
-        for i in 1 2 3; do
-            echo $message && sleep 1.5 && echo "$placeholder" && sleep 1.5
+        i=0; while [ $i -lt 5 ]; do
+            sleep 1 && echo "$message" && sleep 1 && echo "$placeholder"
+            ((i += 1))
         done
     fi
 }
--- setup-tmux-notify.sh
+++ setup-tmux-notify.sh
@@ -4,5 +4,9 @@ current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 prevbg=$(tmux show -g status-right |sed -n 's/.*bg=\([^]]*\)].*/\1/p')
 script="#($current_dir/display-message.sh)"
-tmux set-option -ga status-right \
-    "#[fg=#282a36,bg=${prevbg},nobold,nounderscore,noitalics]#[fg=#ff5555,bg=#282a36] $script "
+status_right_length=$(tmux show -gv status-right-length)
+[ $status_right_length -lt 100 ] && tmux set -g status-right-length 100
+if ! tmux show -gv status-right |grep -q "$script"; then
+    tmux set -ga status-right \
+        "#[fg=#282a36,bg=${prevbg},nobold,nounderscore,noitalics]#[fg=#ff5555,bg=#282a36] $script "
+fi
