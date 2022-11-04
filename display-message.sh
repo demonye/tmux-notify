89fb624e444c9abe800ff4852cae4626becc4b9b
Eric Ye <eric.ye@facteon.global>
2022-11-04 22:17:14 +1300

    Flash as many times as half of status interval

--- display-message.sh
+++ display-message.sh
@@ -5,18 +5,22 @@ source $current_dir/variables.sh
 
 main()
 {
-    message="$(cat $TMUX_NOTIFY_FILE 2>/dev/null)"
-    i=0; placeholder=""
-    while [ $i -lt ${#message} ]; do
-        [ $(echo -n "${message:$i:1}" |wc -c) -eq 1 ] && placeholder+=" " || placeholder+="  "
-        i=$(($i + 1))
+    prev_message=
+    status_interval=$(tmux show -gv status-interval)
+    (( flash_times = status_interval / 2 ))
+    i=0; while [ $i -lt $flash_times ]; do
+        message="$(cat $TMUX_NOTIFY_FILE 2>/dev/null)"
+        [ -z "$message" ] && echo && break
+        if [[ "$message" != "$prev_message" ]]; then
+            j=0; placeholder=; while [ $j -lt ${#message} ]; do
+                [ $(echo -n "${message:$j:1}" |wc -c) -eq 1 ] && placeholder+=" " || placeholder+="  "
+                (( j += 1 ))
+            done
+        fi
+        sleep 1 && echo "$message" && sleep 1 && echo "$placeholder"
+        prev_message="$message"
+        (( i += 1 ))
     done
-    if [ -n "$message" ]; then
-        i=0; while [ $i -lt 5 ]; do
-            sleep 1 && echo "$message" && sleep 1 && echo "$placeholder"
-            ((i += 1))
-        done
-    fi
 }
 
 main
